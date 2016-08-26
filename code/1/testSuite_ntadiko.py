3896ddd6fd88206111b57609314551d06a36bd5d
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-08-26 02:42:30 -0400

    hw1:rel0

--- code/1/testSuite_ntadiko.py
+++
@@ -1,8 +0,0 @@
-import unittest,testBirthdayParadox,testShanonsEntropy
-
-testSuite_ntadiko = unittest.TestSuite()
-testSuite_ntadiko.addTest(unittest.makeSuite(testBirthdayParadox.Test_birthdayparadox))
-testSuite_ntadiko.addTest(unittest.makeSuite(testShanonsEntropy.Test_shanonsentropy))
-
-testSuiteRunner = unittest.TextTestRunner()
-testSuiteRunner.run(testSuite_ntadiko)
