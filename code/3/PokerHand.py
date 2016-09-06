2b6bc1bbce8b499e5780de01f976a0d76a5d1395
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-09-05 21:45:30 -0400

    hw3:dev6

--- code/3/PokerHand.py
+++ code/3/PokerHand.py
@@ -6,10 +6,11 @@ Copyright 2012 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 """
-
+from __future__ import division
 from Card import *
 from collections import Counter
 import sys
+from test_PokerHand import *
 sys.dont_write_bytecode=True
 
 class ClassName(object):
@@ -39,12 +40,12 @@ class PokerHand(Hand):
         """
         self.ranks = Counter([card.rank for card in self.cards])
 
-    def has_pair(self):
+    def has_1_pair(self):
         """Returns True if the hand has a pair of cards with same rank, False otherwise.
         """
         return self._has_NofaRank(2)
 
-    def has_two_pair(self):
+    def has_2_two_pair(self):
         """Returns True if he hand has a twopair, False otherwise.
         """
         if not self.ranks:
@@ -61,18 +62,20 @@ class PokerHand(Hand):
                 return True
         return False
 
-    def has_three_of_a_kind(self):
+    def has_3_three_of_a_kind(self):
         """Returns True if the hand has three cards with the same rank, False otherwise.
         """
         return self._has_NofaRank(3)
 
-    def has_straight(self):
+    def has_4_straight(self):
         """Returns True if the hand has five cards with ranks in sequence (aces can be high or low, so Ace-2-3-4-5 is a straight and so is 10-Jack-Queen-King-Ace, but Queen-King-Ace-2-3 is not.), otherwise False.
         """
         if not self.ranks:
             self.rank_hist()
 
         ranks = self.ranks.keys()
+        if len(ranks)<5:
+            return False
         seq = 1
         for i in xrange(1, len(ranks)):
            seq = (seq + 1) if ranks[i - 1] + 1 == ranks[i] else 1
@@ -86,27 +89,35 @@ class PokerHand(Hand):
                 return True
         return False
 
-    def has_flush(self):
+    def has_5_flush(self):
         """Returns True if the hand has a flush, False otherwise.
 
         Note that this works correctly for hands with more than 5 cards.
         """
         return self._has_NofaSuit(5)
 
-    def has_full_house(self):
+    def has_6_full_house(self):
         """Returns True if the hand has three cards with one rank, two cards with another, otherwise False.
         Note: A Veener method calling self._has_NofaRank(arg)
         """
-        return self._has_NofaRank(3) and self._has_NofaRank(2)
+        twokind,threekind=False,False
+        for k,v in self.ranks.iteritems():
+            if v == 2:
+                twokind=True
+            if v== 3:
+                threekind=True
+            if twokind and threekind:
+                return True
+        return False
 
 
-    def has_four_of_a_kind(self):
+    def has_7_four_of_a_kind(self):
         """Returns True if the hand has four cards with the same rank, otherwise False.
         Note: A Veener method calling self._has_NofaRank(arg)
         """
         return self._has_NofaRank(4)
 
-    def has_straight_flush(self):
+    def has_8_straight_flush(self):
         """Returns True if the hand has five cards in sequence (as defined above) and with the same suit, otherwise False.
         Note: A Veener method calling self._has_NofaRank(arg)
         """
@@ -118,115 +129,47 @@ class PokerHand(Hand):
                 if v is not 1:
                     return False
                return True
+        return False
 
-class CheatDeck(Deck):
-
-    def remove_cards_by_rank(self, rank,n):
-        arr=[]
-        for card in self.cards:
-            if card.rank == rank:
-                arr.append(card)
-                n-=1
-            if n==0:
-                break
-        for card in arr:
-            self.remove_card(card)
-        return arr
-    
-    def move_cards_m_n_duplicates_by_rank(self,hand, m,n,T):
-        for i in xrange(m):
-            card1=self.cards[random.randint(1,len(self.cards)-1)]
-            self.remove_card(card1)
-            hand.cards+=[card1]+self.remove_cards_by_rank(card1.rank, n-1)
-        self.move_cards(hand, T-(m*n))
-
-    def remove_cards_by_suit(self, suit,n):
-        arr=[]
-        for card in self.cards:
-            if n < 0:
-                return arr
-            if card.suit == suit:
-                n-=1
-                arr.append(card)
-        for card in arr:
-            self.remove_card(card)
+    def classify(self):
+        self.labels=[]
+        for method in reversed([ attribute for attribute in dir(self) if "has" == attribute[:3]]):
+            if getattr(self, method)():
+                self.labels.append(method[6:])
+        return self.labels
 
 
-    def move_cards_n_duplicate_by_suit(self,hand,n,T):
-        card1 = self.cards[random.randint(1,len(self.cards))]
-        hand.add_card(card1)
-        hand.cards+=self.remove_cards_by_suit(card1.suit,n-1)
-        self.move_cards(hand, T-n)
-
-    def move_cards_pair(self,hand,n):
-        """Moves two cards with the same rank"""
-        self.move_cards_m_n_duplicates_by_rank(hand, 1, 2, n)
-
-    def move_cards_two_pair(self, hand, n):
-        """Moves two pairs of cards with the same rank"""
-        self.move_cards_m_n_duplicates_by_rank(hand, 2, 2, n)
-
-    def move_cards_three_of_a_kind(self,hand,n):
-        """Moves three cards with the same rank"""
-        self.move_cards_m_n_duplicates_by_rank(hand, 1, 3, n)
-
-    def move_cards_straight(self,hand,n):
-        """Move five cards with ranks in sequence (aces can be high or low, so Ace-2-3-4-5 is a straight and so is 10-Jack-Queen-King-Ace, but Queen-King-Ace-2-3 is not.)"""
-        card1 = self.remove_cards_by_rank(random.randint(1,10), 1)[0]
-        hand.add_card(card1)
-        start=card1.rank
-        for rank in xrange(start+1,start+5):
-            card=self.remove_cards_by_rank(rank, 1)[0]
-            hand.add_card(card)
-        self.move_cards(hand, 2)
-
-    def move_cards_flush(self,hand,n):
-        """Move five cards with the same suit"""
-        self.move_cards_n_duplicate_by_suit(hand, 5, 7)
-
-    def move_cards_full_house(self,hand,n):
-        """Move three cards with one rank, two cards with another"""
-        self.move_cards_m_n_duplicates_by_rank(hand, 1, 3, 3)
-        self.move_cards_m_n_duplicates_by_rank(hand, 1, 2, 4)
-
-    def move_cards_four_of_a_kind(self,hand,n):
-        """Move four cards with the same rank"""
-        self.move_cards_m_n_duplicates_by_rank(hand, 1, 4, n)
-
-    def move_cards_straight_flush(self,hand,n):
-        """Move five cards in sequence (as defined above) and with the same suit"""
-        suit=random.randint(0,3)
-        cds=sorted([ card for card in self.cards if card.suit == suit], key=lambda card: card.rank)
-        arr=[]
-        for card in cds:
-            if len(arr) == 5:
-                for card in arr:
-                    hand.add_card(card)
-                    self.remove_card(card)
-                break
-            if len(arr) == 0:
-                arr.append(card)
-                continue
-            if arr[-1].rank+1 == card.rank:
-                arr.append(card)
-            else:
-                arr=[]
-        self.move_cards(hand, n-5)
-
+def probabilities(trails=1000,N=7):
+    labels=[]
+    for i in xrange(trails):
+        deck=Deck()
+        deck.shuffle()
+        for hand in [ PokerHand() for i in xrange(int(52/N)) ]:
+            deck.move_cards(hand, N)
+            if hand.classify():
+                labels+=hand.labels
+    hist=Counter(labels)
+    total= sum(hist.values())
+    for k,v in hist.iteritems():
+        hist[k]=v/total
+    print sorted([(k,v) for k,v in hist.iteritems()],key=lambda x:  x[1])
 
 
 if __name__ == '__main__':
+    probabilities(trails=1000,N=5)
+    quit()
     # make a deck
     deck=CheatDeck()
     deck.shuffle()
-
     # deal the cards and classify the hands
     for i in range(7):
         hand=PokerHand()
         deck.move_cards_straight_flush(hand, 7)
+        hand.classify()
+        print hand.label
         #hand.sort()
         print hand
-        print hand.has_straight_flush()
+        print hand.has_8_straight_flush()
         print ''
         quit()
 
