11fb5f1d8d526d25d59c04b3dce26edf1f04ec5c
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-09-23 23:54:08 -0400

    hw5:dev3

--- code/5/Osyczka2.py
+++ code/5/Osyczka2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+
 g1 = lambda x1,x2:    x1+x2 >= 2
 g2 = lambda x1,x2:    x1+x2 <= 6
 g3 = lambda x1,x2:    x1-x2 >= 2
@@ -12,3 +13,7 @@ x1x2bounds = xrange(0,11)
 x3x5bounds = xrange(1,6)
 x4bounds = xrange(0,7)
 x5bounds = xrange(0,11)
+
+f1 = lambda x1,x2,x3,x4,x5:  -(25*(x1-2)**2 + (x2-2)**2 + ((x3-1)**2)*((x4-4)**2)+(x5-1)**2)
+f2 = lambda x1,x2,x3,x4,x5,x6:  x1**2 + x2**2 + x3**2 + x4**2 + x5**2 + x6**2
+
