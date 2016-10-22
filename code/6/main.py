a9d3ef19f02075d5e0fa4ec2929702990f634b59
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-10-21 23:28:58 -0400

    hw6:dev1

--- code/6/main.py
+++ code/6/main.py
@@ -7,14 +7,10 @@ __author__ = "ntadiko"
 
 
 from problems import schaffer,osyczka2,kursawe
-from optimizers import sa,mws,de
+from optimizers import sa,mws
 
-# for problem in [schaffer,osyczka2,kursawe]:
-#     for optimizer in [sa,mws]:
-#         problem(optimizer=optimizer()).solve()
-#         print "-"*80
+for problem in [schaffer,osyczka2,kursawe]:
+    for optimizer in [sa,mws]:
+        problem(optimizer=optimizer()).solve()
+        print '\n\n'+"-"*80+'\n\n'
 #can be  solve(), study()
-
-schaffer(optimizer=mws()).solve()
-schaffer(optimizer=sa()).solve()
-#osyczka2(optimizer=mws()).solve()
